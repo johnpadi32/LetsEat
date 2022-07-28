84eec5ebe1926296ebf9cf21019b8ce54152b0d6
John Padilla <johnpadi8790@gmail.com>
2022-07-28 01:58:36 -0600

    Creating the restaurantItem class

---
+++ LetsEat/Map/Model/RestaurantItem.swift
@@ -0,0 +1,54 @@
+//
+//  RestaurantItem.swift
+//  LetsEat
+//
+//  Created by John Padilla on 7/28/22.
+//
+
+import UIKit
+import MapKit
+
+class RestaurantItem: NSObject, MKAnnotation {
+    
+    var name: String?
+    var cuisines: [String] = []
+    var lat: Double?
+    var long: Double?
+    var address: String?
+    var postalCode: String?
+    var state: String?
+    var imageUrl: String?
+    var restaurantID: Int?
+    
+    init(dict: [String:AnyObject]) {
+        if let name = dict["name"] as? String { self.name = name}
+        if let cuisines = dict["cuisines"] as? [String] { self.cuisines = cuisines}
+        if let lat = dict["lat"] as? Double { self.lat = lat }
+        if let long = dict["long"] as? Double { self.long = long}
+        if let address = dict["address"] as? String { self.address = address}
+        if let postalCode = dict["postalCode"] as? String { self.postalCode = postalCode}
+        if let state = dict["state"] as? String { self.state = state}
+        if let imageUrl = dict["imageUrl"] as? String { self.imageUrl = imageUrl}
+        if let restaurantID = dict["restaurantID"] as? Int { self.restaurantID = restaurantID}
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        guard let lat = lat, let long = long else { return CLLocationCoordinate2D() }
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        if cuisines.isEmpty {
+            return "" }
+        else if cuisines.count == 1 {
+            return cuisines.first
+        } else {
+            return cuisines.joined(separator: ", ")
+        }
+    }
+
+}
